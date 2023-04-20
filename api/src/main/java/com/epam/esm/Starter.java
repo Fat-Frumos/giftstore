44548042dd462d84666f535242cf0ba3447a6333
Fat-Frumos <lokankara@i.ua>
2023-04-20 11:31:22 +0300

    Module2 was updated

--- api/src/main/java/com/epam/esm/Starter.java
+++
@@ -1,9 +0,0 @@
-package com.epam.esm;
-
-import com.epam.esm.config.WebAppInitializer;
-
-class Starter {
-    public static void main(String[] args) {
-        WebAppInitializer web = new WebAppInitializer();
-    }
-}
--- api/src/main/java/com/epam/esm/exception/CertificateIsExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.epam.esm.exception;
-
-public class CertificateIsExistsException extends RuntimeException {
-    public CertificateIsExistsException(String message) {
-        super(message);
-    }
-}
